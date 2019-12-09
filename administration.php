a7a20289344bcbd3f9e43d5a7539bfd1c36c1cf4
CalamityJu <juliettel.78@hotmail.fr>
2019-12-09 20:06:35 +0100

    création page suppression membre

--- administration.php
+++ administration.php
@@ -59,25 +59,16 @@
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <form action="" method="post">
-                                <input type="hidden" id="hidden" value="<?= $membre['membre_id'];?>">
+                                <input type="hidden" value="<?= $membre['membre_id'];?>">
                                 <button class="dropdown-item my-0">Modifier rang</button>
                             </form>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                Supprimer
+                            </button>
                             <form action="" method="post">
-                                <input type="hidden" id="hidden" value="<?= $membre['membre_id'];?>">
-                                <button class="dropdown-item my-0">Supprimer</button>
-                            </form>
-                            <form action="" method="post">
-                                <input type="hidden" id="hidden" value="<?= $membre['membre_id'];?>">
+                                <input type="hidden" value="<?= $membre['membre_id'];?>">
                                 <button class="dropdown-item my-0">Bannir</button>
                             </form>
-                            <div class="dropdown-item btn-group dropright">
-                                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropright
-                                </button>
-                                <div class="dropdown-menu">
-                                    <!-- Dropdown menu links -->
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </td>
@@ -87,6 +78,37 @@
             </tbody>
         </table>
     </div>
+
+
+    <!-- MODALS -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <form action="function/suppression_membre.php" method="post">
+                    <input type="hidden" name="supp_membre" value="<?= $membre['membre_id'];?>">
+                    <button class="dropdown-item my-0">Supprimer</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+    <form action="function/suppression_membre.php" method="post">
+        <input type="hidden" name="supp_membre" value="<?= $membre['membre_id'];?>">
+        <button class="dropdown-item my-0">Supprimer</button>
+    </form>
+
+
 
     <div id="admin_forum_page">
         <h2>Coucou forum </h2>
---
+++ function/suppression_membre.php
@@ -0,0 +1,20 @@
+<?php
+
+    // On se connecte à la bdd
+    try {
+        $bdd= new PDO('mysql:host=localhost; dbname=espacemembre; charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    } 
+
+    if(isset($_POST['supp_membre']) && !empty($_POST['supp_membre'])){
+        //Supprimer le membre
+        $req = $bdd->prepare('DELETE FROM membres WHERE membre_id = ?');
+        $req->execute(array($_POST['supp_membre']));
+        $req->closeCursor();
+        header('Location:'.$_SERVER['HTTP_REFERER']);
+        die();
+    } else {
+        echo "Aucun membre n'est sélectionné.";
+    }
+?>
